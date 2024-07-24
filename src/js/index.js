859722955347ead0791ed446ae72b572caa1cdfc
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-07-24 12:03:36 +0300

    css helper comments where added to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 import "../css/fonts.css";
 import "../css/vars.css";
 import "../css/main.css";
-//
-//
-//
-//
+
+// import "../css/navigation.css";
+// import "../css/hero.css";
+// import "../css/about.css";
+// import "../css/services.css";
+// import "../css/cases.css";
 import "../css/team.css";
+// import "../css/production.css";
+// import "../css/ticker.css";
+// import "../css/footer.css";
+
 import "../css/atomic.css";
